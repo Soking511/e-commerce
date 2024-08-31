c04b8747d352423e37b54ff28755720f4e7067b5
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2024-08-31 21:56:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var app = express();
-var port = 3000;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
-app.listen(port, function () {
-    console.log("Example app listening on port ".concat(port));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
