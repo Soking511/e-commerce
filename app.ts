d23638b8ba61b5a6528bb970622598690c8a8cac
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2024-09-04 23:36:39 +0300

    unhandledRejection Errors

--- app.ts
+++ app.ts
@@ -3,18 +3,22 @@ import dotenv from "dotenv";
 import dbConnection from "./config/db";
 import * as all from './Apps/moreInterfaces/index'
 import mountRoutes from "./Apps/index";
+import { Server } from "http";
 
-const app: express.Application = express()
-dotenv.config();
-dbConnection();
-const port = process.env.PORT;
-
+const app: express.Application = express(), port = process.env.PORT
 app.use(express.json());
+dotenv.config();
 
+
+dbConnection();
 mountRoutes(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+let server:Server = app.listen(port, () => console.log(`Example app listening on port ${port}`) );
+
+process.on( 'unhandledRejection', (err:Error) =>{
+  console.error( `unhandledRejection: ${err.name} | ${err.message}` )
+  server.close(() => {
+    console.error('shutting the server down');
+    process.exit(1);
+  })
 })
-
-
--- config/db.ts
+++ config/db.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+
 import dotenv from "dotenv";
 const dbConnection =()=>{
   mongoose.connect( process.env.DB! ) // Link Data Base
-  // mongoose.connect( "mongodb+srv://youseeftareq5176:L5HH4wn7puu2oJMy@cluster0.fjotd.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0" ) // Link Data Base
   .then( ()=> {
     console.log( "Connected!" )
   }).catch(( error )=>{
