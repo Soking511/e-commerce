74a4e5c66a602881af4c69e7a07a6da9e69f90e3
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2024-09-04 23:26:43 +0300

    Handling Errors

--- Apps/httpMethods.ts
+++ Apps/httpMethods.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import asyncHandler from 'express-async-handler';
 import { Model } from "mongoose";
 import { FilterData } from "./moreInterfaces/filterData";
+import APIErrors from "../utils/apiErrors";
 
 export const getAll = <modelType>( model: Model<any>, modelName:string ) =>
   asyncHandler( async( req:Request, res:Response, next:NextFunction ) => {
@@ -16,6 +17,8 @@ export const getAll = <modelType>( model: Model<any>, modelName:string ) =>
 export const getOne = <modelType>( model: Model<any> ) =>
   asyncHandler( async( req:Request, res:Response, next:NextFunction) =>{
     const document: modelType | null = await model.findById(req.params.id);
+    if ( !document ) return next( new APIErrors( `The Document [${req.params.id}] not found`, 404 ) );
+
     res.status(200).json( {data:document} );
   })
 
@@ -28,11 +31,13 @@ export const POST = <modelType>( model: Model<any> ) =>
 export const PUT = <modelType>( model:Model<any> ) =>
   asyncHandler( async( req:Request, res:Response, next:NextFunction ) =>{
     const document: modelType | null = await model.findByIdAndUpdate( req.params.id, req.body, { new: true } );
+    if ( !document ) return next( new APIErrors( `The Document [${req.params.id}] not found`, 404 ) );
     res.status(200).json( {data:document} );
   })
 
 export const DELETE = <modelType>( model:Model<any> ) =>
   asyncHandler( async( req:Request, res:Response, next:NextFunction ) => {
     const document: modelType | null = await model.findByIdAndDelete( req.params.id );
+    if ( !document ) return next( new APIErrors( `The Document [${req.params.id}] not found`, 404 ) );
     res.status(204).json()
   })
--- middlewares/globalErrors.ts
+++ middlewares/globalErrors.ts
@@ -3,7 +3,6 @@ import { CustomErrors } from '../Apps/moreInterfaces/customErrors';
 
 const globalErrors = (err:CustomErrors, req:Request, res:Response, next:NextFunction) => {
     err.statusCode = err.statusCode || 500;
-    err.status = err.status || "Server Error!!!";
     res.status( err.statusCode ).json({
       err,
       message:err.message,
