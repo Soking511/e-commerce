5e64cf4220729e239a0db2ab3903aa058d82e1b3
Soking511 <youseeftareq5176@gmail.com>
2024-09-24 01:32:49 +0300

    get

--- Front/src/app/app.config.ts
+++ Front/src/app/app.config.ts
@@ -4,7 +4,5 @@ import { routes } from './app.routes';
 import { provideHttpClient, withXsrfConfiguration } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(
-    withXsrfConfiguration( { cookieName:'cookies', headerName:'X-CSRF-Token'})
-  )]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
 };
