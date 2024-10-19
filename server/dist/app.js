ab01b93a21e538eab9a328d6d4b4bb8f9a3f69ab
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2024-10-20 01:09:40 +0300

    update

--- server/dist/app.js
+++ server/dist/app.js
@@ -9,7 +9,6 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const compression_1 = __importDefault(require("compression"));
 const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize"));
 const hpp_1 = __importDefault(require("hpp"));
-const helmet_1 = __importDefault(require("helmet"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const db_1 = __importDefault(require("./config/db"));
@@ -17,7 +16,7 @@ const index_1 = __importDefault(require("./Apps/index"));
 const app = (0, express_1.default)();
 dotenv_1.default.config();
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:4200', 'https://e-commerce-rose-five.vercel.app/'],
+    origin: 'https://e-commerce-rose-five.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token', 'X-API-KEY'],
     credentials: true
@@ -34,7 +33,7 @@ app.use(express_1.default.json({ limit: '2kb' }));
 app.use((0, compression_1.default)());
 app.use((0, express_mongo_sanitize_1.default)());
 app.use((0, hpp_1.default)({ whitelist: ['price', 'category', 'subcategory'] }));
-app.use((0, helmet_1.default)({ crossOriginResourcePolicy: { policy: 'same-site' } }));
+// app.use(helmet({ crossOriginResourcePolicy: { policy: 'same-site' } }));
 // app.use(express.static('uploads'));
 app.use('/uploads', express_1.default.static(path_1.default.join(__dirname, 'uploads')));
 app.get('/', (req, res) => {
