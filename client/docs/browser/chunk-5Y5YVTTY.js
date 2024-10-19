cebcd703f9094aaee5cae308208f5ade6d48b1af
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2024-10-19 19:44:38 +0300

    update

--- client/docs/browser/chunk-5Y5YVTTY.js
+++
@@ -1,1 +0,0 @@
-import{m as e}from"./chunk-AJKLIETP.js";import{S as o,X as t}from"./chunk-TXHN3XOK.js";var i=class r{constructor(c){this._GlobalService=c;this.productImages=this._GlobalService.productsImage}productImages="";static \u0275fac=function(a){return new(a||r)(t(e))};static \u0275prov=o({token:r,factory:r.\u0275fac,providedIn:"root"})};export{i as a};
--- client/docs/browser/chunk-DTATLONT.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-3OTKNFOC.js";import"./chunk-T344MZPX.js";import"./chunk-AJKLIETP.js";import"./chunk-TXHN3XOK.js";export{a as OrdersComponent};
