5017c24c706944f6f49814764afff44f5c52ad32
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2024-10-19 19:40:53 +0300

    update

---
+++ client/dist/front/browser/chunk-FWMPF7KL.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-TMDM73FV.js";import"./chunk-D7JRYGJS.js";import"./chunk-FX5KCJE3.js";import"./chunk-TXHN3XOK.js";export{a as OrdersComponent};
---
+++ client/dist/front/browser/chunk-J7V7KOET.js
@@ -0,0 +1,1 @@
+import{m as e}from"./chunk-FX5KCJE3.js";import{S as o,X as t}from"./chunk-TXHN3XOK.js";var i=class r{constructor(c){this._GlobalService=c;this.productImages=this._GlobalService.productsImage}productImages="";static \u0275fac=function(a){return new(a||r)(t(e))};static \u0275prov=o({token:r,factory:r.\u0275fac,providedIn:"root"})};export{i as a};
