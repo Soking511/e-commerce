1b7aac169b1962ec3869364e568c1101accc2f71
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2024-10-19 19:37:56 +0300

    update

--- client/src/app/core/services/global.service.ts
+++ client/src/app/core/services/global.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 
 export class GlobalService {
-  baseURL:string = "http://localhost:3300/";
+  baseURL:string = "https://ashoq-ser.onrender.com/";
   version:string = 'api/v1'
   authRoute: string = 'api/v1/auth';
   productsRoute: string = 'api/v1/products';
