cae9cb69b2cbe468a2bb38b5abe05c38682f759d
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2024-10-24 19:13:19 +0300

    Delete server/dist directory

--- server/dist/Apps/coupons/couponsModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const couponsSchema = new mongoose_1.Schema({
-    name: { type: String, required: true, trim: true, unique: true },
-    discount: { type: Number, required: true, min: 1, max: 100 },
-    expireTime: { type: Date, required: true },
-}, { timestamps: true });
-exports.default = (0, mongoose_1.model)('coupons', couponsSchema);
